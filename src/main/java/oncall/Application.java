d7d400b1b867a4f28c63462b5050082474d98a14
Park Yu Han <pbk2312@naver.com>
2024-12-11 00:22:39 +0900

    feat(model) - 로직 수행

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,10 @@
 package oncall;
 
+import oncall.controller.SheduleController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        SheduleController sheduleController = new SheduleController();
+        sheduleController.run();
     }
 }
